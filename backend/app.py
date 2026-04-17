86264cd0efeab461bf48e5ff381e35aedecb8d3a
rohitmaneshwar <rohitmaneshwar12@gmail.com>
2026-04-17 13:45:29 +0530

    update app.py file

--- backend/app.py
+++ backend/app.py
@@ -356,7 +356,9 @@ def get_reviews():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all() 
-        print(f"✅ Database Tables Checked/Created at: {db_path}")
-        
+        print("✅ Database Tables Checked/Created in PostgreSQL Cloud Successfully!")
+    
+    app.run(debug=True) 
+            
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
